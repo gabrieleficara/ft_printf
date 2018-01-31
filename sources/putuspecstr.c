ab03845c32386ce94c68f408abde418695671a37
Gabriele FICARA <gficara@e1r3p13.42.fr>
2018-01-31 16:05:09 +0100

    pustrcorr

--- sources/putuspecstr.c
+++ sources/putuspecstr.c
@@ -6,7 +6,7 @@
 /*   By: gficara <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/25 17:13:26 by gficara           #+#    #+#             */
-/*   Updated: 2018/01/31 13:26:10 by gficara          ###   ########.fr       */
+/*   Updated: 2018/01/31 16:04:37 by gficara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	uniwrite(int let, int len)
 			ft_putchar((len != 4) ? 224 | (15 & (let >> 12)) :
 				240 | (7 & (let >> 18)));
 		else
-			ft_putchar(192 | (63 & (let >> 6)));
+			ft_putchar(192 | (31 & (let >> 6)));
 		if (len > 3)
 			ft_putchar(128 | (63 & (let >> 12)));
 		if (len > 2)
