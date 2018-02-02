3d75f3f78f976503347dd580c822eb89c27644c1
Gabriele FICARA <gficara@e1r3p13.42.fr>
2018-02-02 12:56:34 +0100

    hello

--- sources/putuspecstr.c
+++ sources/putuspecstr.c
@@ -6,7 +6,7 @@
 /*   By: gficara <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/25 17:13:26 by gficara           #+#    #+#             */
-/*   Updated: 2018/02/02 11:57:18 by gficara          ###   ########.fr       */
+/*   Updated: 2018/01/31 15:12:33 by gficara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 void	uniwrite(int let, int len)
 {
 	if (len == 1)
-		return (ft_putchar((unsigned int)let));
+		return (ft_putchar(let));
 	else
 	{
 		if (len > 2)
-			ft_putchar((unsigned int)(len != 4) ? 224 | (15 & (let >> 12)) :
-				240 | (let >> 18));
+			ft_putchar((len != 4) ? 224 | (15 & (let >> 12)) :
+				240 | (7 & (let >> 18)));
 		else
-			ft_putchar((unsigned int)192 | (31 & (let >> 6)));
+			ft_putchar(192 | (63 & (let >> 6)));
 		if (len > 3)
-			ft_putchar((unsigned int)128 | (63 & (let >> 12)));
+			ft_putchar(128 | (63 & (let >> 12)));
 		if (len > 2)
-			ft_putchar((unsigned int)128 | (63 & (let >> 6)));
+			ft_putchar(128 | (63 & (let >> 6)));
 		if (len > 1)
-			ft_putchar((unsigned int)128 | (63 & let));
+			ft_putchar(128 | (63 & let));
 	}
 }
 
@@ -53,7 +53,7 @@ char	*mincor(char *tmp, t_flags flags)
 		tmp[i] = '0';
 		tmp[0] = '-';
 	}
-	else if (flags.pre >= len && flags.dot == 1)
+	else if (flags.pre >= len)
 	{
 		tmp[i] = '0';
 		tmp = ft_sfstrjoin("-", tmp, 2);
